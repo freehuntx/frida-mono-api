615a6d45f3357df8dd845af784e8402c857d7a4f
Alexandre Beaulieu <alex@segfault.me>
2020-01-28 14:07:53 -0500

    fix: Handle multiple mono module names.

--- src/mono-module.js
+++ src/mono-module.js
@@ -1,9 +1,22 @@
-let monoModule = Process.findModuleByName('mono.dll')
+const KNOWN_RUNTIMES = ['mono.dll', 'libmonosgen-2.0.so' ];
+const KNOWN_EXPORTS = ['mono_thread_attach'];
 
-if (!monoModule) {
-  const monoThreadAttach = Module.findExportByName(null, 'mono_thread_attach')
-  if (monoThreadAttach) monoModule = Process.findModuleByAddress(monoThreadAttach)
+let monoModule = null;
+
+// Look for a known runtime module.
+for (let x of KNOWN_RUNTIMES) {
+    let module = Process.findModuleByName(x);
+    if (module) {
+	monoModule = module;
+	break;
+    }
 }
-if (!monoModule) throw new Error('Cant find mono!')
+
+// Look for a known mono export.
+if (!monoModule) {
+    const monoThreadAttach = Module.findExportByName(null, 'mono_thread_attach')
+    if (monoThreadAttach) monoModule = Process.findModuleByAddress(monoThreadAttach)
+}
+if (!monoModule) throw new Error('Can\'t find Mono runtime!')
 
 export default monoModule
